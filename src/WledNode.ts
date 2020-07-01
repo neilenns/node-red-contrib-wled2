987f2890ef4db5a6615554148a2f5a25e27016ee
Neil Enns <info@danecreekphotography.com>
2020-07-01 06:51:50 -0700

    Switch to using request-promise-native

--- src/WledNode.ts
+++ src/WledNode.ts
@@ -7,35 +7,53 @@ import IWledState from "./types/IWLedState";
 import WledDevice from "./WledDevice";
 
 export default class WledNode extends NodeRedNode {
-  private _wled: WledDevice;
-  private _config: INodeConfig;
+  private wled: WledDevice;
+  private config: INodeConfig;
 
-  private setState(msg: any, send: any, done: any) {
+  constructor(config: INodeConfig) {
+    super();
+    global.RED.nodes.createNode(this, config);
+
+    if (!config.address) {
+      this.error("No WLED device address specified");
+      return;
+    }
+
+    this.wled = new WledDevice({ server: config.address });
+    this.config = config;
+
+    this.on("input", msg => this.setState(msg));
+    this.wled.on("connected", () => this.onConnected());
+    this.wled.on("disconnected", () => this.onDisconnected());
+
+    this.wled.getState();
+  }
+
+  private setState(msg: any) {
     const { payload } = msg;
     const state = {
-      on: payload.color ?? (this._config.state ? Boolean(this._config.state) : undefined),
+      on: payload.color ?? (this.config.state ? Boolean(this.config.state) : undefined),
       seg: [
         {
           col: [
-            payload.color1 ?? helpers.hexToRgb(this._config.color1),
-            payload.color2 ?? helpers.hexToRgb(this._config.color2),
-            payload.color3 ?? helpers.hexToRgb(this._config.color3),
+            payload.color1 ?? helpers.hexToRgb(this.config.color1),
+            payload.color2 ?? helpers.hexToRgb(this.config.color2),
+            payload.color3 ?? helpers.hexToRgb(this.config.color3),
           ],
-          fx: payload.effect ?? Number(this._config.effect),
+          fx: payload.effect ?? Number(this.config.effect),
           id: 0,
         } as IWledSegment,
       ],
     } as IWledState;
 
-    this._wled.setState(state);
+    this.wled.setState(state);
   }
 
-  constructor(config: INodeConfig) {
-    super();
-    global.RED.nodes.createNode(this, config);
-    this._wled = new WledDevice({ server: config.address });
-    this._config = config;
+  private onConnected() {
+    this.status({ fill: "green", shape: "dot", text: `Connected: ${this.wled.server}` });
+  }
 
-    this.on("input", (msg: any, send: any, done: any) => this.setState(msg, send, done));
+  private onDisconnected() {
+    this.status({ fill: "red", shape: "dot", text: "Disconnected" });
   }
 }
