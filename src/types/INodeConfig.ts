ac7a1c6f5ee31ae3dae68e5c5e8ee68a9c4d1533
danecreekphotography <info@danecreekphotography.com>
2020-07-01 11:37:08 -0700

    Intensity, speed, and brightness can be set (#6)

--- src/types/INodeConfig.ts
+++ src/types/INodeConfig.ts
@@ -1,9 +1,12 @@
 export default interface INodeConfig {
   address: string;
+  brightness: number;
   color1: string; // Comes in as #aabbcc
   color2: string; // Comes in as #aabbcc
   color3: string; // Comes in as #aabbcc
   effect: number;
+  effectIntensity: number;
+  effectSpeed: number;
   name: string;
   state: string
   delay: number;
